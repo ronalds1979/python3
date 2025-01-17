4c10b3e082be847d220a7982c583faa679fc4cdd
ronalds1979 <ronaldo.souza@gmail.com>
2025-01-17 17:01:50 +0100

    new2

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
+Salutation ='Hello'
+Space = ' '
+Subject = 'World'
+whole_string = (Salutation + ',' + Space + Subject)
+print(whole_string)
 
-print('Hello world')
+print(Subject)
+print('The first letter of ' + Salutation + ' is ' + Salutation[0] + ', because 0 represents the first letter of the string')
+
+# Iterate through items in a range
+for i in range(0, 5):
+    print('Number: {}'.format(i))
+
+name = input ('What is your name? ')
+thing = input ('What is your favorite activity?')
+print ('Miss ' + name + ' favourite activity is ' + thing)
